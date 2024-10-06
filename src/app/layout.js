217ca56effbf84e23cd166ed17bd77bb04856097
LedFed <strugov7@mail.ru>
2024-10-06 19:14:55 +0500

    last update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,6 @@ import { CartProvider } from "./context/CartProvider";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
