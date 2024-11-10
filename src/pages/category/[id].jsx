48a6b7949f3ec9c0e12ee440f58f3a56864d811c
LedFed <strugov7@mail.ru>
2024-11-11 04:53:44 +0500

    fix products 6

--- src/pages/category/[id].jsx
+++ src/pages/category/[id].jsx
@@ -7,7 +7,7 @@ import "@/app/globals.css";
 
 export async function getStaticPaths() {
     try {
-        const response = await fetch(`${process.env.API_HOST}/products`)
+        const response = await fetch(`api/products`)
         const data = await response.json();
         console.log(data);
 
@@ -26,7 +26,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     try {
-        const response = await fetch(`${process.env.API_HOST}/categoriy?value=${encodeURIComponent(params.id)}`);
+        const response = await fetch(`api/categoriy?value=${encodeURIComponent(params.id)}`);
         const data = await response.json();
         console.log(data);
         const product = data.rows;
