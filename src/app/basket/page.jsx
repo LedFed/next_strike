b3fa26183fe21cd4d7356f23fbffbfbd6db0753a
LedFed <strugov7@mail.ru>
2025-03-10 01:02:18 +0500

    alpha -v7 redirect fix

--- src/app/basket/page.jsx
+++ src/app/basket/page.jsx
@@ -30,7 +30,7 @@ export default function page() {
     };
 
     try {
-      const response = await fetch('http://localhost:3000/api/telegram', {
+      const response = await fetch('/api/telegram', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
